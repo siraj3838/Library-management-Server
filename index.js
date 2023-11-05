fe3401e31ac27f871be642c991c6e35585cf4e0a
Sirajul Islam <ayasabegum134@gmail.com>
2023-11-05 21:54:04 +0600

    I Create Get Method from server side because i need Category Data

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5005;
+
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hq29e8f.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const categoryCollection = client.db('categoryDB').collection('category');
+    const booksCollection = client.db('categoryDB').collection('allBook')
+
+    // category get
+    app.get('/bookCategory', async(req, res)=> {
+        const result = await categoryCollection.find().toArray();
+        res.send(result)
+        // console.log(result)
+    })
+    // books post
+    app.post('/books', async(req, res)=> {
+        
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res)=>{
+    res.send('Library Server Is Running');
+})
+app.listen(port, ()=>{
+    console.log(`My Library Running From ${port}`)
+})
+
+
+
+
